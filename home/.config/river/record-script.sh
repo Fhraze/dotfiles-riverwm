3beac63cf040dd14ccecc8d336273ab8b2d9deb9
Fhraze <76172824+Fhraze@users.noreply.github.com>
2024-01-26 21:20:26 +0000

    fix record-script.sh

--- home/.config/river/record-script.sh
+++ home/.config/river/record-script.sh
@@ -5,7 +5,7 @@ cd ~/Videos/recordings
 if [[ "$(pidof wf-recorder)" == "" ]]; then
     notify-send "wf-recorder" "Starting recording" -a 'wf-recorder'
     paplay --volume=45536 ~/.config/river/coolblep.wav &
-    wf-recorder --audio=alsa_output.usb-Kingston_HyperX_Cloud_Flight_Wireless-00.analog-stereo.monitor -f './recording_'"$(date '+%Y_%m_%_d..%H.%M')"'.mp4' -t --geometry "$(slurp)" 
+    wf-recorder -f './recording_'"$(date '+%Y_%m_%_d..%H.%M')"'.mp4' -t --geometry "$(slurp)" 
 else
     /usr/bin/kill --signal SIGINT wf-recorder
     notify-send "wf-recorder" "Recording Stopped" -a 'wf-recorder'
